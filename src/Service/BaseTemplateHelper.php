ae5670db797c31a17598137003418428898eb97b
Anthony Poon <ypoon196@gmail.com>
2018-07-26 17:32:48 +0800

    BaseTemplateHelper will now be configurated in constructor instead of yaml

--- src/Service/BaseTemplateHelper.php
+++ src/Service/BaseTemplateHelper.php
@@ -9,23 +9,15 @@ class BaseTemplateHelper {
     private $title = "Web Application";
     private $jsParam = [];
     private $params;
+
     public function __construct(ParameterBagInterface $paramsBag, RouterInterface $router) {
-        $this->params = $paramsBag->get("side_menu");
-        foreach ($this->params as $p) {
-            $item = [
-                "text" => $p["text"],
-                "icon" => $p["icon"] ?? null,
-                "url" => $router->generate($p["route"]),
-            ];
-            if ($p["child"] ?? false) {
-                $item["child"][] = [
-                    "text" => $p["child"]["text"],
-                    "icon" => $p["child"]["icon"] ?? null,
-                    "url" => $router->generate($p["child"]["route"])
-                ];
-            }
-            $this->sideMenu[] = $item;
-        }
+        $this->sideMenu = [
+        	[
+        		"text" => "Home",
+				"icon" => "home",
+				"url" => $router->generate("home")
+			]
+		];
     }
 
     /**
