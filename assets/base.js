481495a4170472bcf191b9c082adcdffb838389a
Anthony Poon <ypoon196@gmail.com>
2018-08-21 16:34:59 +0800

    Added layout config option. Revamped top nav bar

--- assets/base.js
+++ assets/base.js
@@ -1,20 +1,10 @@
 global.$ = global.jQuery = require('jquery');
 import CRUDTable from "./share/crud_table";
 import axios from 'axios';
+import _ from 'underscore';
 
 
 $(document).ready(function(){
     // Toggle navbar menu via selector specified in data-submenu attr
-    $("#top-navbar *[data-submenu]").click(function() {
-        let target = $($(this).data("submenu"));
-        let leftOffset = $(this).offset().left - target.outerWidth() + $(this).outerWidth();
-        target.css("left", leftOffset);
-        target.show();
-    });
-    // Hide menu when clicking outside of data-submenu
-    $(document).click(function(evt){
-        if (!$(evt.target).is(".submenu") && !$(evt.target).is("#top-navbar *[data-submenu]") && $(".submenu").has(evt.target).length == 0) {
-            $(".submenu").hide();
-        }
-    });
+
 });
