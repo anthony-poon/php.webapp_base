24b011442b190f6f058938b6523ba4043f6e1812
Anthony Poon <ypoon196@gmail.com>
2018-10-30 10:45:46 +0800

    More removal v2

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,6 @@ Encore
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
-    .addEntry("demo/gallery/index", [
-        "./assets/gallery/index.scss",
-        "./assets/gallery/index.js"
-    ])
     .addEntry("base", [
         "./assets/base.js",
         "./assets/base.scss",
